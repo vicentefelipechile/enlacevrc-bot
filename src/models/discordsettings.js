1eb8513e5277ab6d9bfeb7742211b1f821e6ebdc
Vicente <70909800+vicentefelipechile@users.noreply.github.com>
2025-10-03 21:54:43 -0300

    feat: add Discord server settings definitions for loading and saving settings

---
+++ src/models/discordsettings.js
@@ -0,0 +1,13 @@
+/**
+ * @file        models/discordsettings.js
+ * @author      vicentefelipechile
+ * @description Discord settings definitions for loading and saving settings
+ */
+
+const DISCORD_SERVER_SETTINGS = {
+    VERIFICATION_ROLE: 'verification-role',
+    VERIFICATION_PLUS_ROLE: 'verification-plus-role',
+    AUTO_NICKNAME: 'auto-nickname',
+}
+
+module.exports = DISCORD_SERVER_SETTINGS;
